991c3c5d334a6e7d5f236ec66ff20ef0f82a33b5
marcobeffa <marcobeffa@gmail.com>
2025-08-12 04:12:50 +0200

    fix test

--- test/controllers/list_items_controller_test.rb
+++ test/controllers/list_items_controller_test.rb
@@ -1,38 +1,46 @@
 require "test_helper"
 
 class ListItemsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get list_items_index_url
-    assert_response :success
+  setup do
+    @list_item = list_items(:one) # Assicurati di avere un fixture chiamato :one
   end
 
-  test "should get show" do
-    get list_items_show_url
+  test "should get index" do
+    get list_items_url
     assert_response :success
   end
 
   test "should get new" do
-    get list_items_new_url
+    get new_list_item_url
     assert_response :success
   end
 
-  test "should get create" do
-    get list_items_create_url
+  test "should create list_item" do
+    assert_difference("ListItem.count") do
+      post list_items_url, params: { list_item: { title: "Nuovo Item", content: "Descrizione di test" } }
+    end
+    assert_redirected_to list_item_url(ListItem.last)
+  end
+
+  test "should show list_item" do
+    get list_item_url(@list_item)
     assert_response :success
   end
 
   test "should get edit" do
-    get list_items_edit_url
+    get edit_list_item_url(@list_item)
     assert_response :success
   end
 
-  test "should get update" do
-    get list_items_update_url
-    assert_response :success
+  test "should update list_item" do
+    patch list_item_url(@list_item), params: { list_item: { title: "Titolo aggiornato" } }
+    assert_redirected_to list_item_url(@list_item)
   end
 
-  test "should get destroy" do
-    get list_items_destroy_url
-    assert_response :success
+  test "should destroy list_item" do
+    assert_difference("ListItem.count", -1) do
+      delete list_item_url(@list_item)
+    end
+    assert_redirected_to list_items_url
   end
 end
